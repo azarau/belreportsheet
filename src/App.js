6922ae2a6815b630781b67e0e7d86f3bd5c40a07
@Azara'u <77005734+azarau@users.noreply.github.com>
2022-04-05 13:04:15 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div style={{ textAlign: "center" }}>
+        <img src={logo} style={{ height: 70 }} />
+      </div>
+      <h1>Brilliant Esystems Academy</h1>
+      <h2>End of the Programm Report Sheet</h2>
+    </div>
+  );
+}
---
+++ src/App1.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+//import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img src="https://avatars.mds.yandex.net/i?id=8e799af5da5f6dc41460ce6c951e74ba-5233014-images-thumbs&n=13"></img>
+    </div>
+  );
+}
